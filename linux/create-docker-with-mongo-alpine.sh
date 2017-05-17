d26fdb8ff0c27e638da50332a89acb6434272a45
Wanderley Mayhe Junior <juniormayhe@gmail.com>
2017-05-17 19:28:04 -0300

    fixed scripts

--- linux/create-docker-with-mongo-alpine.sh
+++ linux/create-docker-with-mongo-alpine.sh
@@ -1,15 +1,15 @@
 ﻿#!/bin/bash
-LOCALHOST_PATH=/some/content
-CONTAINER_NAME=webserver
-DOCKERIZED_HOSTNAME=meu-servidor
+LOCALHOST_PATH=/some/data
+CONTAINER_NAME=banco
+DOCKERIZED_HOSTNAME=meu-banco
 clear
 echo "**********************************************"
-echo "Assuming you will use nginx:alpine official image,"
-echo "this script will create a new docker container with nginx,"
-echo "accept tcp connection from all ports and point your"
+echo "Assuming you will use mvertes/alpine-mongo image,"
+echo "this script will create a new docker container with mongodb,"
+echo "accept tcp connection from 27017 port ports and point your"
 printf $"local application path $LOCALHOST_PATH to docker's /usr/share/nginx/html"
 echo "**********************************************"
-docker run --name $CONTAINER_NAME -v $LOCALHOST_PATH:/usr/share/nginx/html:ro -t -h $DOCKERIZED_HOSTNAME -P -d nginx:alpine nginx -g 'daemon off;'
+docker run --name $CONTAINER_NAME -v $LOCALHOST_PATH:/data/db -t -h $DOCKERIZED_HOSTNAME -p 27017:27017 -d mvertes/alpine-mongo
 
 #wanna go back to tty?
 echo "Entering bash in Alpine"
