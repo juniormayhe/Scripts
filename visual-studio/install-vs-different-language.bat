07e8af3de69a42a19712270034de415d257582e2
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2017-11-15 17:07:29 -0500

    Create install-vs-different-language.bat

---
+++ visual-studio/install-vs-different-language.bat
@@ -0,0 +1,5 @@
+@echo off
+echo First download Visual Studio at https://www.visualstudio.com/thank-you-downloading-visual-studio/?sku=Community&rel=15
+echo then execute the installer passing the locale parameter
+
+vs_community__785288071.1510782919.exe --locale en-US
