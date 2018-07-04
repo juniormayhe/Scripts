5658577f68685b47b842284a6ff5e735ba5f5b82
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2018-07-04 08:26:01 -0500

    Update loop-table-and-split-items.sql

--- sql-server/loop-table-and-split-items.sql
+++ sql-server/loop-table-and-split-items.sql
@@ -3,19 +3,19 @@ DECLARE @emails TABLE (
     RecipientName varchar(200), 
     Email varchar(70)
 )
-
+--supposing you have unique ids, we could get each item from a table
 DECLARE @id int = 0
 WHILE(1 = 1)
 BEGIN
 	SELECT @id = MIN(Id)
-	FROM [INFOSMART2].Gerencial.dbo.ITAccount WHERE Id > @id
+	FROM [SERVER].Catalog.dbo.Emails WHERE Id > @id
 	IF @id IS NULL 
 		BREAK
 	
 	DECLARE @recipientName varchar(200) = '',@email varchar(100) = ''
 	--emails could be separated by comma, transform it to semicolon
 	SELECT @recipientName=[name], @email = REPLACE(Email,',',';') 
-  FROM [SERVER].Catalog.dbo.Emails 
+	FROM [SERVER].Catalog.dbo.Emails 
 	WHERE Id = @id
 	
 	INSERT INTO @emails 
