f9adeaad271e04f9ecb410b064881450c88dea2a
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2018-07-19 17:30:12 -0500

    Update cygwin-rsync-copy-files-from-linux.bat

--- windows/cygwin-rsync-copy-files-from-linux.bat
+++ windows/cygwin-rsync-copy-files-from-linux.bat
@@ -9,6 +9,9 @@ SETLOCAL
 SET CWRSYNCHOME=C:\cygwin64
 SET PATH=%CWRSYNCHOME%\BIN;%PATH%
 
-ECHO Copy data to a root folder named TargetFolder on Drive C
+ECHO Copy data to a root folder named TargetFolder on Drive C (this will not work with too many files)
 ECHO on local server generate ssh-keygen -t rsa and paste id_rsa.pub content into remote server's /root/.ssh/authorized_keys
 REM rsync -trzh --progress --stats -p --chmod=ugo=rwX root@192.168.0.7:/var/spool/asterisk/monitor/* /cygdrive/c/TargetFolder
+
+REM Since * will expand to multiple files this can throw too many arguments error on rsync, the workaround is to avoid * and filter results
+find /var/spool/asterisk/monitor/ | grep .gsm | rsync -trzh --progress --stats -p --chmod=ugo=rwX root@192.168.0.7:/var/spool/asterisk/monitor/$1 /cygdrive/c/TargetFolder
