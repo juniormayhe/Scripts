52d67e4f40665624335eb3a50f11e18cdd2cd98d
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2018-08-15 10:43:34 -0500

    Create wget-onedrive-file.sh

---
+++ linux/wget-onedrive-file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## at Onedrive, generate a share link and visit it
+## replace this URL https://onedrive.live.com/?authkey=YOURAUTHKEY&cid=YOURCID&id=YOURRESOURCEID&parId=root&o=OneUp with:
+
+wget 'https://onedrive.live.com/download?cid=YOURCID&resid=YOURRESOURCEID&authkey=YOURAUTHKEY'
