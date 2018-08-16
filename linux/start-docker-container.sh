21a0809ba1fa6b0bf77828425672be0efd5c1092
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2018-08-16 10:01:56 -0500

    Update start-docker-container.sh

--- linux/start-docker-container.sh
+++ linux/start-docker-container.sh
@@ -2,3 +2,6 @@
 # if netcore Program.cs contains .UseUrls("http://*:5100"), the internal port within container will be 5100, external can be the same or other
 echo "starting docker... 8080 port should be free at hosting OS, internal port will be 5100
 docker run -p 8080:5100 --rm --name myapp company/imagename
+
+#to start TWO docker containers use (UseUrls must be defined in Program.cs of both NET Core WebApi and Web projects): 
+#docker run -p 5100:5100 --rm --name myapi company/myapi & docker run -p 8080:8080 --rm --name mywebapp company/mywebapp 
