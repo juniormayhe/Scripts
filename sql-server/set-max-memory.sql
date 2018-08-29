2bcc38d4cf9a51b25e22885b4fc8fc4152285d90
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2018-08-29 11:34:02 -0500

    Update and rename set-maxx-memory.sql to set-max-memory.sql

---
+++ sql-server/set-max-memory.sql
@@ -0,0 +1,29 @@
+
+-- recommended settings
+--GB	MB	    Recommended Setting	
+--16	16384	  12288	
+EXEC sys.sp_configure 'max server memory (MB)', '12288'; RECONFIGURE;
+
+--32	32768	29491	
+EXEC sys.sp_configure 'max server memory (MB)', '29491'; RECONFIGURE;
+
+--64	65536	58982	
+EXEC sys.sp_configure 'max server memory (MB)', '58982'; RECONFIGURE;
+
+--128	131072	117964	
+EXEC sys.sp_configure 'max server memory (MB)', '117964'; RECONFIGURE;
+
+--256	262144	235929	
+EXEC sys.sp_configure 'max server memory (MB)', '235929'; RECONFIGURE;
+
+--512	524288	471859	
+EXEC sys.sp_configure 'max server memory (MB)', '471859'; RECONFIGURE;
+
+--1024	1048576	943718	
+EXEC sys.sp_configure 'max server memory (MB)', '943718'; RECONFIGURE;
+
+--2048	2097152	1887436	
+EXEC sys.sp_configure 'max server memory (MB)', '1887436'; RECONFIGURE;
+
+--4096	4194304	3774873	
+EXEC sys.sp_configure 'max server memory (MB)', '3774873'; RECONFIGURE;
