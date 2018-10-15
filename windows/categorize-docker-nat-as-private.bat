799b4edd869d0a5d4c676e8f061ba550050f822a
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2018-10-15 11:59:41 +0100

    Update categorize-docker-nat-as-private.bat

--- windows/categorize-docker-nat-as-private.bat
+++ windows/categorize-docker-nat-as-private.bat
@@ -3,3 +3,4 @@ echo This avoids Docker firewall message when DockerNAT network category is set 
 echo we must change it to Private...
 echo Please run this as administrator
 powershell Set-NetConnectionProfile -interfacealias \"vEthernet (DockerNAT)\" -NetworkCategory Private
+echo Now you can share again C drive in Docker for Windows.
