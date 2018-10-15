4ae86eae680f36484d43de0687b8bcea626d152f
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2018-10-15 11:59:00 +0100

    Create categorize-docker-nat-as-private.bat

---
+++ windows/categorize-docker-nat-as-private.bat
@@ -0,0 +1,5 @@
+@echo off
+echo This avoids Docker firewall message when DockerNAT network category is set Internal in HyperV
+echo we must change it to Private...
+echo Please run this as administrator
+powershell Set-NetConnectionProfile -interfacealias \"vEthernet (DockerNAT)\" -NetworkCategory Private
